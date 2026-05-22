3d9cdbdd67bce340eaec4e891db1650bdee80580
EliMacBook <ekaansah@gmail.com>
2026-05-22 12:16:36 -0400

    accidental deletion of imports

--- src/landingPageComponents/Gameboard.jsx
+++ src/landingPageComponents/Gameboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
 import "./Gameboard.css";
